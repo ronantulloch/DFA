b58cc6b7c3156336b28735365ae203496e23e5f3
Ronan Tulloch <Ronantulloch@gmail.com>
2022-08-16 13:35:28 +0930

    update

--- driver.m
+++ driver.m
@@ -1,21 +1,5 @@
 %Define Event Log
-A = ["a", "b", "B", "Bb5"];
-
-% To demonstrate lack of cycles
-%A = ["aaaaa","abbb"];
-
-% To demonstrate one-to-one-ness
-%A = ["aaaaa","abaaa"];
-
-
-% Number of states: maximum case
-%A = ["abc","def","ghi"];
-% Number of states: minimum case
-%A = ["a","a","a"];
-
-
-% Number of final states
-%A = ["abc","def","aef","abc"];
+A = ["a", "b", "B", "Bb"];
 
 
 %Get DFA
